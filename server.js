dff1fc21d00be5800d3d29c7795c5598e72356d4
Shannon Yazdani <shannonyazdani@gmail.com>
2022-09-08 19:50:33 -0400

    changed mysql to mysql2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-const mysql = require("mysql2");
+const mysql = require("mysql");
 
 const cors = require("cors");
 app.use(cors());
