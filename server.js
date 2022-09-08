0062a422fccdb3a525d4f0f64f9bad9989e404d8
Shannon Yazdani <shannonyazdani@gmail.com>
2022-09-08 19:48:46 -0400

    add images

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 const cors = require("cors");
 app.use(cors());
