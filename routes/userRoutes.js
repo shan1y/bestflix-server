5631272c4a5c1982c19ab7ac59188a0741f46821
Shannon Yazdani <shannonyazdani@gmail.com>
2022-09-19 21:52:12 -0400

    had to fix query

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -22,13 +22,13 @@ router.post(
     } else {
       const { email, password } = req.body;
       let sqlCredCheck = `SELECT * FROM heroku_bf65c7f5a682285.users WHERE email='${email}'`;
-      db.query(sqlCredCheck, (err, result) => {
+      db.db.query(sqlCredCheck, (err, result) => {
         if (result.length === 1) {
           return res.status(200).json({ errors: "Account already exists" });
         } else {
           let sql = `INSERT INTO heroku_bf65c7f5a682285.users (email, password)
           VALUES ('${email}','${password}');`;
-          db.query(sql, (err, result) => {
+          db.db.query(sql, (err, result) => {
             if (err) throw err;
             res.send(result);
           });
